04d345f3d4409ddaa42a00c25b62856e29eb8cf4
Aisyah Farhanah <92865748+AisyahFarhanah07@users.noreply.github.com>
2021-12-31 21:02:47 +1200

    Delete js1 directory

--- js1/login.js
+++
@@ -1,9 +0,0 @@
-function han() {
-    let far = document.getElementById("far").value
-    let af = document.getElementById("af").value
-
-    if(far == "Aisyah" && af == "kelaperan"){
-        location.href= "berhasilLogIn.html";
-    }else
-    alert("wrong password");
-}
